127f7ffb1db3d323805b906716e2cc89ddcd8bff
Aiden Chang <skylineciel@gmail.com>
2021-06-29 12:12:55 -0400

    completed card.rb

---
+++ memory_puzzle/card.rb
@@ -0,0 +1,38 @@
+class Card
+    attr_reader :face_value
+
+    def initialize(face_value)
+        @face_value = face_value
+        @face_down = true
+    end
+
+    def display
+        if !@face_down
+            return @face_value
+        end
+    end
+
+    def hide
+        @face_down = true
+    end
+
+    def reveal
+        @face_down = false
+    end
+
+    def to_s
+        @face_value.to_s
+    end
+
+    def ==(card_instance)
+        @face_value == card_instance.face_value
+    end
+end
+
+p card = Card.new(:A)
+p card.display
+p card.reveal 
+p card.display
+p card2 = Card.new(:B)
+p card2 == card
+p card2.to_s
